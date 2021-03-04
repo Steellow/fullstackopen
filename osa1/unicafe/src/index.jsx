e19420036ece386a732f066ef6b4631ffd8462e8
Steellow <hankilapio@gmail.com>
2021-03-04 10:41:29 +0200

    Complete 1.6

--- osa1/unicafe/src/index.jsx
+++ osa1/unicafe/src/index.jsx
@@ -7,7 +7,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h2>Statistics</h2>
+      <Stats text="good" amount={good} />
+      <Stats text="neutral" amount={neutral} />
+      <Stats text="bad" amount={bad} />
+    </div>
+  );
 };
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Stats = ({ text, amount }) => (
+  <p>
+    {text} {amount}
+  </p>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
