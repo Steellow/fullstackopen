23be25d2f817d73ad5971f5682d7dfa6b25e3f7c
Steellow <hankilapio@gmail.com>
2021-03-07 14:07:31 +0200

    Complete 2.13

--- osa2/maiden-tiedot/src/components/CountryList.jsx
+++ osa2/maiden-tiedot/src/components/CountryList.jsx
@@ -1,14 +1,34 @@
 import React from "react";
+import { useState } from "react";
+import CountryDetails from "./CountryDetails";
 
 const CountryList = ({ countries }) => {
-  console.log(countries);
-  return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.alpha2Code}>{country.name}</li>
-      ))}
-    </ul>
-  );
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const selectCountry = (country) => {
+    setSelectedCountry(country);
+  };
+
+  if (selectedCountry == null) {
+    return (
+      <ul>
+        {countries.map((country) => (
+          <li key={country.alpha2Code}>
+            {country.name} <button onClick={() => selectCountry(country)}>show</button>
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    return (
+      <div>
+        <button style={{ marginTop: "30px" }} onClick={() => selectCountry(null)}>
+          back to results
+        </button>
+        <CountryDetails country={selectedCountry} />
+      </div>
+    );
+  }
 };
 
 export default CountryList;
